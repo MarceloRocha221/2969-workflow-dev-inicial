4ddfd01c1e065fa3937f13ab662aa5255d652a2e
Marcelo Rocha <marcelorocha.mfy@gmail.com>
2025-10-29 10:57:32 -0300

    feat: add: métodos para listar livros por autor

--- populate.sql
+++ populate.sql
@@ -7,10 +7,10 @@ CREATE TABLE autores(
 );
 
 INSERT INTO autores (nome, nacionalidade) 
-VALUES  ("JRR Tolkien", "sul-africano"),
-        ("Ursula LeGuin", "estadunidense"),
-        ("Machado de Assis", "brasileira"),
-        ("Júlio Cortázar", "argentino");
+VALUES  ('JRR Tolkien', 'sul-africano'),
+        ('Ursula LeGuin', 'estadunidense'),
+        ('Machado de Assis', 'brasileira'),
+        ('Júlio Cortázar', 'argentino');
 
 CREATE TABLE editoras(
   id      INTEGER NOT NULL PRIMARY KEY,
@@ -22,17 +22,17 @@ CREATE TABLE editoras(
 );
 
 INSERT INTO editoras (nome, cidade, email) 
-VALUES  ("Europa-América", "Lisboa", "e@e.com"),
-        ("Morro Branco", "São Paulo", "m@m.com"),
-        ("Aleph", "São Paulo", "al@al.com"),
-        ("Ateliê", "São Paulo", "a@a.com");
+VALUES  ('Europa-América', 'Lisboa', 'e@e.com'),
+        ('Morro Branco', 'São Paulo', 'm@m.com'),
+        ('Aleph', 'São Paulo', 'al@al.com'),
+        ('Ateliê', 'São Paulo', 'a@a.com');
 
 CREATE TABLE livros(
-  id          INTEGER NOT NULL PRIMARY KEY,
-  titulo      TEXT    NOT NULL,
-  paginas     INTEGER NOT NULL,
-  editora_id  INTEGER NOT NULL,
-  autor_id    INTEGER NOT NULL,
+  id            INTEGER NOT NULL PRIMARY KEY,
+  titulo        TEXT    NOT NULL,
+  paginas       INTEGER NOT NULL,
+  editora_id    INTEGER NOT NULL,
+  autor_id      INTEGER NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (editora_id) REFERENCES editoras (id),
@@ -41,11 +41,10 @@ CREATE TABLE livros(
 
 INSERT INTO livros (titulo, paginas, autor_id, editora_id)
 VALUES 
-   ("O Hobbit", 230, 1, 1),
-   ("O Silmarillion", 400, 1, 1),
-   ("O Silmarillion", 400, 1, 1),
-   ("O Feiticeiro de Terramar", 450, 2, 2),
-   ("Os Despossuídos", 300, 2, 3),
-   ("Memórias Póstumas de Brás Cubas", 150, 3, 4);
-  
+    ('O Hobbit', 230, 1, 1),
+    ('O Silmarillion', 400, 1, 1),
+    ('O Feiticeiro de Terramar', 450, 2, 2),
+    ('Os Despossuídos', 300, 2, 3),
+    ('Memórias Póstumas de Brás Cubas', 150, 3, 4);
+ 
 .exit
